33d701d18b8a41e05d46a5db140399477cb4a390
yefan-paskarcastan <efanov.pa.a@gmail.com>
2021-07-06 18:53:19 +0300

    Добавлена модель Manager

---
+++ SoftTradeTestAvicom/Models/Manager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SoftTradeTestAvicom.Models
+{
+    public class Manager
+    {
+        public int Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ SoftTradeTestAvicom/Models/SoftTradeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftTradeTestAvicom.Models
+{
+    public class SoftTradeDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\SQLEXPRESS;Initial Catalog=SoftTrade;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Manager> Managers { get; set; }
+    }
+}
