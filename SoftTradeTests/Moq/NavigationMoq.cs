c27358615bd3b20eb91feba974311a42df3ec0a8
yefan-paskarcastan <efanov.pa.a@gmail.com>
2021-07-10 11:40:11 +0300

    Тесты для менеджера

---
+++ SoftTradeTests/Moq/NavigationMoq.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using SoftTradeTestAvicom.Utils;
+
+namespace SoftTradeTests.Moq
+{
+    public class NavigationMoq : INavigationManager
+    {
+        public void Navigate(NavigationInput arg)
+        {
+            
+        }
+
+        public void Register<TViewModel, TView>(TViewModel viewModel, string navigationKey)
+            where TViewModel : class
+            where TView : FrameworkElement
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
